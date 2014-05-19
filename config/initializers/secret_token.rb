c61d6f69b660552f6cdd2063fb302a37fb2b2852
Chu Anh Tuan <chu.anh.tuan@framgia.com>
2014-05-19 09:39:34 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mms2::Application.config.secret_key_base = 'a149cf2189684ba91415c4d4170e837c3445dffe8325765cf97d2f170004d3b8074447081c68075d9b3e27cee337649c91aec86c73b0917801e8b21e221dfe25'
