fe5f5278af95c9962a6fb990df58bd8224ebfd19
Anh Tranngoc <tran.ngoc.anh@framgia.com>
2014-05-19 12:52:30 +0700

    Make framwork, homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page base
+  def full_title(page_title)
+    base_title = "Member Management System"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+   end
+
 end
