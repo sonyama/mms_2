6f4238579637b857ddd08d3a1854d7ec90efa796
nguyen.thanh.tungb <nguyen.thanh.tungb@framgia.com>
2014-05-29 10:16:16 +0700

    Import CSV

--- app/controllers/admin/csvs_controller.rb
+++ app/controllers/admin/csvs_controller.rb
@@ -1,15 +1,33 @@
 class Admin::CsvsController < ApplicationController
+  
   def export
-    CSV.generate do |csv|
-      model = params[:model].constantize
-      #if params[:model]=='users'
-        
-        csv << model.column_names
-        User.all.each do |user|
-          csv << user.attributes.values_at(*model.column_names)
-        end
-      #end
-    end
-    render text: CSV
+    model = params[:model].constantize
+    send_data export_csv(model), filename: "#{model.table_name}.csv"    
   end
+  def import
+    model = params[:model].constantize
+    file  = params[:file]
+    if file.nil?
+      flash[:error] = "Please choose file to import."
+    else
+      import_csv model, file
+      flash[:success] = "Import completed."
+    end
+    redirect_to admin_csvs_path
+  end
+  private
+    def import_csv model, file
+      CSV.foreach(file.path, headers: true) do |row|
+        records = model.where id: row[0]
+        model.create row.to_hash if records.empty?
+      end
+    end
+    def export_csv model
+      CSV.generate do |csv|
+        csv << model.column_names
+        model.all.each do |item|
+          csv << item.attributes.values_at(*model.column_names)
+        end
+      end
+    end
 end
